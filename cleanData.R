5765f335bfc746669a08a638e6c8c67c15fc8c7a
aramsdev <aramsdev@gmail.com>
2023-11-14 22:08:51 -0600

    Advance

--- cleanData.R
+++ cleanData.R
@@ -1,22 +1,56 @@
 install.packages('tidyr');
+install.packages('dplyr');
+install.packages('ggplot2');
+install.packages(c("tidyverse"))
+library(tidyverse)
 library(tidyr)
 library(dplyr)
+library(ggplot2)
+
+###################################################
 
 data <- read.csv("data.csv");
 head(data);
 dim(data)
 class(data)
-
 sum(is.na(data))
 
-mean(data$price)
+###################################################
 
-typeof(data$price)
 data <- unique(data);
 
 clean_data <- na.omit(data)
-dim(clean_data);
+summary(clean_data)
+dim(clean_data)
+
+attach(clean_data)
+
+clean_data$bed <- as.integer(clean_data$bed)
+clean_data$bed <- as.integer(clean_data$bed)
+clean_data <- select(clean_data, -prev_sold_date)
+
+###################################################
 
 
+## Histograma distribucion precio
+
+set.seed(123)
+
+price_histogram <- ggplot(clean_data$price, aes(x = price)) +
+  geom_histogram(fill = "lightblue", color = "black", bins = 30) +
+  labs(title = "Histogram of Price", x = "Price", y = "Frequency") +
+  theme_minimal()
 
 
+print(price_histogram)
+
+###################################################
+
+
+pairs(~ bed + bath + acre_lot + house_size + price + zip_code, data = clean_data, gap = 0.4, cex.labels = 4)
+
+m1 <- lm(price ~ bed + bath)
+summary(m1)
+
+plot(m1$fitted.values, price, xlab = "Valores ajustados", ylab = "Price")
+abline(lsfit(m1$fitted.values, price))
